baee815446fa05e61728b9465a59945ba6954570
Dipesh <dipeshgtm@gmail.com>
2013-01-23 09:48:53 +0545

    added host and username/pass

--- application.rb
+++ application.rb
@@ -14,7 +14,7 @@ class Audio < Sinatra::Base
   dbfile = File.expand_path('../screen.sqlite3', __FILE__)
 
   # set :database, "sqlite3:///#{dbfile}"
-  set :database, {adapter: "postgresql", database: "dacj6mbjmmajvd"}
+  set :database, {adapter: "postgresql", database: "dacj6mbjmmajvd", :host => "ec2-54-243-180-196.compute-1.amazonaws.com", :username => "furdplkjqaxcpy",:password => "ZTiqdWdS4axhAMlusal2k35Le4", port: 5432}
   set :app_file, __FILE__
   set :root, File.dirname(__FILE__)
   set :views, "views"
